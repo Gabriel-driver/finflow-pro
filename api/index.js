5775421653a5a194047224d63c7c465fdf4c286a
Gabriel Driver <gabriel.driver@example.com>
2026-03-21 22:02:34 -0300

    Simplify API handler for Vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
-import '../server.js';
-
-// Import the Express app from server.js
-// The server.js exports the app as default
 import app from '../server.js';
 
-// Export as serverless function for Vercel
 export default app;
+
